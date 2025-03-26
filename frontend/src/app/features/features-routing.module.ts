ed720697be8eeed5894eb2c5bf2106476524d9fa
subhajyoti-prusty <subhajyoti.2004@gmail.com>
2025-03-26 09:33:12 +0530

    environment setup

--- frontend/src/app/features/features-routing.module.ts
+++ frontend/src/app/features/features-routing.module.ts
@@ -1,12 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [
-  {
-    path: 'dashboard',
-    loadChildren: () => import('./dashboard/dashboard.module').then(m => m.DashboardModule)
-  }
-];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
